a2e374a68345e32818e6f3add730108026ad88f9
cchimboooo <66074971+cchimbooo@users.noreply.github.com>
2022-01-25 20:01:22 -0300

    feat: adiciona main e .env.example

---
+++ bin/http/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"testentopus/src/armazenamento/mongoDb"
+	"testentopus/src/comunicacao/ginController"
+	"testentopus/src/core/corePessoas"
+	"testentopus/src/mensageria/rabbitMq"
+)
+
+func main() {
+	if loadError := godotenv.Load(); loadError != nil {
+		log.Fatalln(loadError.Error())
+	}
+
+	armazenamento, errArmazenamento := mongoDb.MongoDbFactory(
+		os.Getenv("DBBANCO"),
+		os.Getenv("DBCOLLECTION"),
+		os.Getenv("DBUSUARIO"),
+		os.Getenv("DBSENHA"),
+		os.Getenv("DBHOST"),
+		os.Getenv("DBPORTA"),
+		os.Getenv("DBOPCOES"),
+	)
+	if errArmazenamento != nil {
+		log.Fatalln(errArmazenamento.Error())
+	}
+
+	mensageria, errMensageria := rabbitMq.RabbitMqFactory(
+		os.Getenv("RABBITFILA"),
+		os.Getenv("RABBITUSER"),
+		os.Getenv("RABBITPWD"),
+		os.Getenv("RABBITHOST"),
+		os.Getenv("RABBITPORT"),
+		os.Getenv("RABBITVHOST"),
+	)
+	if errMensageria != nil {
+		log.Fatalln(errMensageria.Error())
+	}
+
+	core, errCore := corePessoas.PessoaCoreFactory(armazenamento, mensageria)
+	if errCore != nil {
+		log.Fatalln(errCore.Error())
+	}
+	ginController.InicializarRotas(core)
+}
